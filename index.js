861297a72d0f9dee1895a1c2d33e0112ac19c832
Red Bill <36853894+redbillis@users.noreply.github.com>
2021-09-18 11:30:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ let generateScales = () => {
     })
 
     xScale = d3.scaleLinear()
-                .domain([minYear, maxYear + 1])
+                .domain([minYear, maxYear + 1]) // My heat map should have multiple tick labels on the x-axis with the years between 1754 and 2015.
                 .range([padding, width - padding])
 
     yScale = d3.scaleTime()
-                .domain([new Date(0,0,0,0, 0, 0, 0), new Date(0,12,0,0,0,0,0)])
+                .domain([new Date(0, 0, 0, 0, 0, 0, 0), new Date(0, 12, 0, 0, 0, 0, 0)])    // My heat map should have multiple tick labels on the y-axis with the full month name.
                 .range([padding, height - padding])
 }
 
@@ -43,11 +43,13 @@ let drawCanvas = () => {
 
 let drawCells = () => {
 
+    // My heat map should have rect elements with a class="cell" that represent the data.
     svg.selectAll('rect')
         .data(values)
         .enter()
         .append('rect')
         .attr('class','cell')
+        // There should be at least 4 different fill colors used for the cells.
         .attr('fill', (item) => {
             let variance = item['variance']
             if(variance <= -1){
@@ -60,6 +62,8 @@ let drawCells = () => {
                 return 'Crimson'
             }
         })
+        // Each cell will have the properties data-month, data-year, data-temp containing their corresponding month, year, and temperature values.
+        // The data-month, data-year of each cell should be within the range of the data.
         .attr('data-year', (item) => {
             return item['year']
         })
@@ -69,12 +73,14 @@ let drawCells = () => {
         .attr('data-temp', (item) => {
             return baseTemp + item['variance']
         })
+        // My heat map should have cells that align with the corresponding month on the y-axis.
         .attr('height', (item)=> {
             return (height - (2 * padding)) / 12
         })
         .attr('y', (item) => {
             return yScale(new Date(0, item['month']-1, 0, 0, 0, 0, 0))
         })
+        // My heat map should have cells that align with the corresponding year on the x-axis.
         .attr('width', (item) => {
             let minYear = d3.min(values, (item) => {
                 return item['year']
@@ -95,9 +101,7 @@ let drawCells = () => {
             tooltip.transition()
                 .style('visibility', 'visible')
             
-            let monthNames = ["January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-            ]
+            let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
         
             tooltip.text(item['year'] + ' ' + monthNames[item['month'] -1 ] + ' : ' + item['variance'])
 
@@ -115,8 +119,9 @@ let generateAxes = () => {
     let xAxis = d3.axisBottom(xScale)
                     .tickFormat(d3.format('d'))
 
+    // My heat map should have multiple tick labels on the y-axis with the full month name.
     let yAxis = d3.axisLeft(yScale)
-                    .tickFormat(d3.timeFormat('%B'))
+                    .tickFormat(d3.timeFormat('%B'))    // B = Full Month (January) and b = Short Month (Jan).
 
     svg.append('g')
         .call(xAxis)
